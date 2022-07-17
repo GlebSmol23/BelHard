1114591114575c680d49750df91a4883a779a661
GlebSmol23 <107396279+GlebSmol23@users.noreply.github.com>
2022-07-17 15:54:40 +0300

    Add files via upload

---
+++ 9_2.py
@@ -0,0 +1,13 @@
+numbers: list[list[int]] = [
+    [1, -2, 3, -4, 5],
+    [1, -2, -3, 4, -5],
+    [-1, 2, -3, -4, -5],
+    [1, -2, 3, -4, 5],
+    [-1, -2, -3, -4, -5],
+]
+
+def get_abs_sum_negative_num(numbers: list[list[int]]) -> int:
+    count: int = 0
+    for raw in numbers:
+        count += sum(filter(lambda number: number < 0 and number % 2, raw))
+    return count
