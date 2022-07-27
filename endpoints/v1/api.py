b723d724e9f421c02f85e608696382801ded0fed
GlebSmol23 <107396279+GlebSmol23@users.noreply.github.com>
2022-07-27 23:23:09 +0300

    Add files via upload

---
+++ endpoints/v1/api.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from.endpoints import category_router, product_router, order_item_router, order_router
+
+
+api_v1_router = APIRouter(
+    prefix="/api/1"
+)
+api_v1_router.include_router(category_router)
+api_v1_router.include_router(product_router)
+api_v1_router.include_router(order_item_router)
+api_v1_router.include_router(order_router)
